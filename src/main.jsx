f33000daac4d6c47112faf72b1f001f395faeef3
Krypto-420 <dan.e.solis88@gmail.com>
2026-03-24 23:23:42 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
